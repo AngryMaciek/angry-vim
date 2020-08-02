805324ccd1a8ac7a7a26e02ed0572f4b7ca124ae
Maciej Bak <wsciekly.maciek@gmail.com>
2020-08-02 10:23:17 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -18,12 +18,11 @@ cd $HOME
 mv .vimrc .vimrc-backup
 
 # link the new dotfile
-ln -s angry-vim/vimrc .vimrc
+ln -s $HOME/angry-vim/vimrc .vimrc
 
 # set monokai color theme
 mkdir -p .vim/colors
-ln -s angry-vim/vim-monokai-master/colors/monokai.vim .vim/colors/monokai.vim
+ln -s $HOME/angry-vim/vim-monokai-master/colors/monokai.vim .vim/colors/monokai.vim
 
 # install vim-airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
-
+git clone https://github.com/AngryMaciek/vim-airline .vim/pack/packages/start/vim-airline
